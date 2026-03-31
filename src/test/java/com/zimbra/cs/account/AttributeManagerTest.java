c1b0d0275480227ae98d737d015ebd7a861ecaed
zextrasGiovanniDefacci <66628670+zextrasGiovanniDefacci@users.noreply.github.com>
2026-03-31 15:02:16 +0200

    chore: introduce grace period ldap attributes (#25)
    * chore: introduce carbonioOtpGracePeriodEnabled and carbonioOtpGracePeriodEndingTime
    
    * chore: bump to version 4.28.0
    
    * chore: add migrations

--- src/test/java/com/zimbra/cs/account/AttributeManagerTest.java
+++ src/test/java/com/zimbra/cs/account/AttributeManagerTest.java
@@ -30,6 +30,6 @@ class AttributeManagerTest {
 
   private void assertLoadedAllAttributes(AttributeManager attributeManager) {
     final Map<String, AttributeInfo> allAttrs = attributeManager.getAttrs();
-    assertEquals(1868, allAttrs.size());
+    assertEquals(1870, allAttrs.size());
   }
 }
